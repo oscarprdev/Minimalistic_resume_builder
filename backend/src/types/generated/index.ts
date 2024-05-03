4d9de730f11e27f226b9fbcecb9cc29b6c93b049
oscarprdev <vlcoscar8@gmail.com>
2024-05-03 21:08:23 +0200

    Add api-spec scafolding

---
+++ backend/src/types/generated/index.ts
@@ -0,0 +1,7 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type { Error } from './models/Error';
+export type { Id } from './models/Id';
