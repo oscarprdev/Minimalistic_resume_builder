8b3f4bc1ddcf314164c60a18eb0fbfd65fdc1afa
oscarprdev <vlcoscar8@gmail.com>
2024-05-15 15:39:59 +0200

    Add cors middleware

---
+++ backend/src/features/core/domain/middlewares/cors.ts
@@ -0,0 +1,40 @@
+import { RequestParams } from '../interfaces';
+
+const corsHeaders = {
+	'Access-Control-Allow-Origin': 'http://localhost:3000',
+	'Access-Control-Allow-Methods': 'GET, OPTIONS, POST, PUT, DELETE',
+	'Access-Control-Allow-Headers': '*',
+};
+const allowedOrigins = ['http://localhost:3000'];
+
+const corsMiddleware = (handler: (request: RequestParams) => Promise<Response>) => {
+	return async (request: RequestParams): Promise<Response> => {
+		const origin = request.headers.get('Origin');
+
+		const headers = { ...corsHeaders };
+		if (origin && allowedOrigins.includes(origin)) {
+			headers['Access-Control-Allow-Origin'] = origin;
+		}
+
+		if (request.method === 'OPTIONS') {
+			return new Response(null, {
+				status: 204,
+				headers,
+			});
+		}
+
+		const { body, status, headers: responseHeaders } = await handler(request);
+
+		const updatedResponse = new Response(body, {
+			status,
+			headers: {
+				...responseHeaders,
+				...headers,
+			},
+		});
+
+		return updatedResponse;
+	};
+};
+
+export { corsMiddleware, corsHeaders };
--- backend/src/features/resume/domain/router/index.ts
+++ backend/src/features/resume/domain/router/index.ts
@@ -1,6 +1,7 @@
 import { Router, RouterType } from 'itty-router';
 import { RouterStrategy } from '../../../core/domain/interfaces';
 import { ResumeApplication } from '../../application';
+import { corsMiddleware } from '../../../core/domain/middlewares/cors';
 
 const RESUME_COMMON_PATH = '/resume/:userId/:resumeId';
 
@@ -32,54 +33,113 @@ export class ResumeRouter implements RouterStrategy {
 
 	private globalRouter() {
 		const globalRouterPath = `/resume/:userId`;
-		this.internalRouter.get(`${globalRouterPath}/list`, (req) => this.resumeApplication.globalUsecase().listResume().handleRequest(req));
-		this.internalRouter.post(`${globalRouterPath}/:resumeId/update`, (req) =>
-			this.resumeApplication.globalUsecase().updateResume().handleRequest(req)
+		this.internalRouter.get(
+			`${globalRouterPath}/list`,
+			corsMiddleware((req) => this.resumeApplication.globalUsecase().listResume().handleRequest(req))
 		);
-		this.internalRouter.delete(`${globalRouterPath}/:resumeId/delete`, (req) =>
-			this.resumeApplication.globalUsecase().deleteResume().handleRequest(req)
+		this.internalRouter.post(
+			`${globalRouterPath}/:resumeId/update`,
+			corsMiddleware((req) => this.resumeApplication.globalUsecase().updateResume().handleRequest(req))
+		);
+		this.internalRouter.delete(
+			`${globalRouterPath}/:resumeId/delete`,
+			corsMiddleware((req) => this.resumeApplication.globalUsecase().deleteResume().handleRequest(req))
 		);
 	}
 
 	private headerRouter() {
 		const headerPath = `${RESUME_COMMON_PATH}/header`;
-		this.internalRouter.get(headerPath, (req) => this.resumeApplication.headerUsecase().describeHeader().handleRequest(req));
-		this.internalRouter.post(headerPath, (req) => this.resumeApplication.headerUsecase().createHeader().handleRequest(req));
-		this.internalRouter.delete(headerPath, (req) => this.resumeApplication.headerUsecase().deleteHeader().handleRequest(req));
+		this.internalRouter.get(
+			headerPath,
+			corsMiddleware((req) => this.resumeApplication.headerUsecase().describeHeader().handleRequest(req))
+		);
+		this.internalRouter.post(
+			headerPath,
+			corsMiddleware((req) => this.resumeApplication.headerUsecase().createHeader().handleRequest(req))
+		);
+		this.internalRouter.delete(
+			headerPath,
+			corsMiddleware((req) => this.resumeApplication.headerUsecase().deleteHeader().handleRequest(req))
+		);
 	}
 
 	private summaryRouter() {
 		const summaryPath = `${RESUME_COMMON_PATH}/summary`;
-		this.internalRouter.get(summaryPath, (req) => this.resumeApplication.summaryUsecase().describeSummary().handleRequest(req));
-		this.internalRouter.post(summaryPath, (req) => this.resumeApplication.summaryUsecase().createSummary().handleRequest(req));
-		this.internalRouter.delete(summaryPath, (req) => this.resumeApplication.summaryUsecase().deleteSummary().handleRequest(req));
+		this.internalRouter.get(
+			summaryPath,
+			corsMiddleware((req) => this.resumeApplication.summaryUsecase().describeSummary().handleRequest(req))
+		);
+		this.internalRouter.post(
+			summaryPath,
+			corsMiddleware((req) => this.resumeApplication.summaryUsecase().createSummary().handleRequest(req))
+		);
+		this.internalRouter.delete(
+			summaryPath,
+			corsMiddleware((req) => this.resumeApplication.summaryUsecase().deleteSummary().handleRequest(req))
+		);
 	}
 
 	private experienceRouter() {
 		const experiencePath = `${RESUME_COMMON_PATH}/experience`;
-		this.internalRouter.get(experiencePath, (req) => this.resumeApplication.experienceUsecase().describeExperience().handleRequest(req));
-		this.internalRouter.post(experiencePath, (req) => this.resumeApplication.experienceUsecase().createExperience().handleRequest(req));
-		this.internalRouter.delete(experiencePath, (req) => this.resumeApplication.experienceUsecase().deleteExperience().handleRequest(req));
+		this.internalRouter.get(
+			experiencePath,
+			corsMiddleware((req) => this.resumeApplication.experienceUsecase().describeExperience().handleRequest(req))
+		);
+		this.internalRouter.post(
+			experiencePath,
+			corsMiddleware((req) => this.resumeApplication.experienceUsecase().createExperience().handleRequest(req))
+		);
+		this.internalRouter.delete(
+			experiencePath,
+			corsMiddleware((req) => this.resumeApplication.experienceUsecase().deleteExperience().handleRequest(req))
+		);
 	}
 
 	private educationRouter() {
 		const educationPath = `${RESUME_COMMON_PATH}/education`;
-		this.internalRouter.get(educationPath, (req) => this.resumeApplication.educationUsecase().describeEducation().handleRequest(req));
-		this.internalRouter.post(educationPath, (req) => this.resumeApplication.educationUsecase().createEducation().handleRequest(req));
-		this.internalRouter.delete(educationPath, (req) => this.resumeApplication.educationUsecase().deleteEducation().handleRequest(req));
+		this.internalRouter.get(
+			educationPath,
+			corsMiddleware((req) => this.resumeApplication.educationUsecase().describeEducation().handleRequest(req))
+		);
+		this.internalRouter.post(
+			educationPath,
+			corsMiddleware((req) => this.resumeApplication.educationUsecase().createEducation().handleRequest(req))
+		);
+		this.internalRouter.delete(
+			educationPath,
+			corsMiddleware((req) => this.resumeApplication.educationUsecase().deleteEducation().handleRequest(req))
+		);
 	}
 
 	private languagesRouter() {
 		const languagesPath = `${RESUME_COMMON_PATH}/languages`;
-		this.internalRouter.get(languagesPath, (req) => this.resumeApplication.languagesUsecase().describeLanguages().handleRequest(req));
-		this.internalRouter.post(languagesPath, (req) => this.resumeApplication.languagesUsecase().createLanguages().handleRequest(req));
-		this.internalRouter.delete(languagesPath, (req) => this.resumeApplication.languagesUsecase().deleteLanguages().handleRequest(req));
+		this.internalRouter.get(
+			languagesPath,
+			corsMiddleware((req) => this.resumeApplication.languagesUsecase().describeLanguages().handleRequest(req))
+		);
+		this.internalRouter.post(
+			languagesPath,
+			corsMiddleware((req) => this.resumeApplication.languagesUsecase().createLanguages().handleRequest(req))
+		);
+		this.internalRouter.delete(
+			languagesPath,
+			corsMiddleware((req) => this.resumeApplication.languagesUsecase().deleteLanguages().handleRequest(req))
+		);
 	}
 
 	private skillsRouter() {
 		const skillsPath = `${RESUME_COMMON_PATH}/skills`;
-		this.internalRouter.get(skillsPath, (req) => this.resumeApplication.skillsUsecase().describeSkills().handleRequest(req));
-		this.internalRouter.post(skillsPath, (req) => this.resumeApplication.skillsUsecase().createSkills().handleRequest(req));
-		this.internalRouter.delete(skillsPath, (req) => this.resumeApplication.skillsUsecase().deleteSkills().handleRequest(req));
+		this.internalRouter.get(
+			skillsPath,
+			corsMiddleware((req) => this.resumeApplication.skillsUsecase().describeSkills().handleRequest(req))
+		);
+		this.internalRouter.post(
+			skillsPath,
+			corsMiddleware((req) => this.resumeApplication.skillsUsecase().createSkills().handleRequest(req))
+		);
+		this.internalRouter.delete(
+			skillsPath,
+			corsMiddleware((req) => this.resumeApplication.skillsUsecase().deleteSkills().handleRequest(req))
+		);
 	}
 }
