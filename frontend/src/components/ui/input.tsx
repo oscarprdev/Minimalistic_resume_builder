0bc703e5271a1fb19ff2869ff9c5a0057c83454d
oscarprdev <vlcoscar8@gmail.com>
2024-05-16 07:11:14 +0200

    Add input variants

--- frontend/src/components/ui/input.tsx
+++ frontend/src/components/ui/input.tsx
@@ -1,25 +1,44 @@
-import * as React from "react"
+import * as React from 'react';
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils';
+import { cva, type VariantProps } from 'class-variance-authority';
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+const inputVariants = cva(
+	'flex caret-purple_200 h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500  disabled:cursor-not-allowed disabled:opacity-50',
+	{
+		variants: {
+			variant: {
+				default: 'border-none shadow-none focus-visible:outline-none',
+				common: 'border-slate-200 shadow-sm focus-visible:ring-1 focus-visible:ring-slate-950',
+			},
+			kind: {
+				default: 'text-[1em]',
+				label: 'text-[0.85em]',
+				text: 'text-[0.9em]',
+				subtitle: 'text-[1.1em]',
+				sectionTitle: 'text-[1.5em]',
+				title: 'text-[2em]',
+			},
+		},
+		defaultVariants: {
+			variant: 'default',
+			kind: 'default',
+		},
+	}
+);
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-9 w-full rounded-md border border-slate-200 bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
-Input.displayName = "Input"
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement>, VariantProps<typeof inputVariants> {}
 
-export { Input }
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, variant, kind, type, ...props }, ref) => {
+	return (
+		<input
+			type={type}
+			className={cn(inputVariants({ variant, kind, className }))}
+			ref={ref}
+			{...props}
+		/>
+	);
+});
+Input.displayName = 'Input';
+
+export { Input };
