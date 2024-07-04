c1dbde5ce8d0a1d0a697f2e12d1d888ab3801e76
oscarprdev <vlcoscar8@gmail.com>
2024-07-04 20:25:21 +0200

    add api_url env

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:8787';
+export const API_URL = process.env.API_URL;
 export const BUCKET_URL = process.env.S3_API_URL || '';
 export const BUCKET_KEY_ID = process.env.S3_ACCESS_KEY_ID || '';
 export const BUCKET_ACCES_KEY = process.env.S3_SECRET_ACCESS_KEY || '';
