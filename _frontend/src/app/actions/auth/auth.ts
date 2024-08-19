8dea74f392abd45ac2e42022556a3a29663cd599
oscarprdev <vlcoscar8@gmail.com>
2024-08-19 17:23:16 +0200

    feat: delete old frontend folder

--- _frontend/src/app/actions/auth/auth.ts
+++
@@ -1,9 +0,0 @@
-'use server';
-
-import { User } from 'next-auth';
-import { loginUser } from './login-user';
-import { logoutUser } from './logout-user';
-
-export const authAction = async (user?: User) => {
-	user ? await logoutUser() : await loginUser({ username: 'oscarpr', password: '1234' });
-};
